705d8f8067401b135213e52fc5fe9874a6e2c8a1
seifessam60 <166764479+seifessam60@users.noreply.github.com>
2024-12-15 14:35:12 +0200

    changes

--- components/StartupCard.jsx
+++ components/StartupCard.jsx
@@ -6,9 +6,8 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Author, Startup } from "@/sanity/types";
 
-export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
-const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+const StartupCard = ({ post }) => {
   const {
     _createdAt,
     views,
